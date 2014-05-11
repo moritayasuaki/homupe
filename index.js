6617002da3c1d4bafc081905a6e59464c5b9eb69
homupe no kannrinin <zousandazou@gmail.com>
2014-05-11 14:32:59 +0000

    markdown poem generator

--- index.js
+++ index.js
@@ -9,30 +9,59 @@ var connect = require("connect"),
     connectRoute = require("connect-route"),
     io = require("socket.io"),
     http = require("http");
+    marked = require("marked");
 
 var Schema = mongoose.Schema;
 
 var CounterSchema = new Schema({
   count: { type: Number, "default": 1 }
 });
+var Counter = mongoose.model('Counter', CounterSchema);
+
 var AccessSchema = new Schema({
   session: String,
   date: Date
 });
-
-
 var Access = mongoose.model('Access', AccessSchema);
-var Counter = mongoose.model('Counter', CounterSchema);
+
+var PoemSchema = new Schema({
+  author: String,
+  text: String,
+  created: { type: Date, default: Date.now },
+  updated: { type: Date, default: Date.now }
+});
+
+var Poem = mongoose.model('Poem', PoemSchema);
 
 (function(root, port) {
   var db = mongoose.connect(dbserver);
   var route = connectRoute(function (router) {
     router.get('/api/counter', function(req, res, next) {
       Counter.findOne({}, function(err, counter){
-        if (err) res.end(err);
+        if (err) console.log(err);
         res.end(JSON.stringify(counter.count));
       });
     }); 
+    router.get('/api/relaypoem', function(req, res, next){
+      Poem.find({}).sort({created:"asc"}).exec(function(err,poems) {
+        if (err) return console.log(err);
+        var text = "";
+        for (var i = 0; i < poems.length; i++) {
+          text += poems[i].text;
+          text += "\n";
+        }
+        res.end(marked(text));
+      });
+    });
+    router.post('/api/relaypoem', function(req, res, next){
+      poem = new Poem();
+      poem.author = req.body.author;
+      poem.text = req.body.text;
+      poem.save( function(err)  {
+        if (err) return console.log(err);
+      });
+      res.end(JSON.stringify(poem));
+    });
   });
   var httpServer = connect()
     .use(require("compression")())
@@ -57,12 +86,19 @@ var Counter = mongoose.model('Counter', CounterSchema);
 
   var sockServer = io.listen(httpServer);
   var members = {};
+  var hash2list = function(hash) {
+    var list = [];
+    for (var key in hash) {
+      list.push(hash[key]);
+    }
+    return list;
+  };
   sockServer.sockets.on('connection', function(sockClient){
     console.log(sockClient.id);
     sockClient.on('set name', function(name) {
       sockClient.set('name', name);
       members[sockClient.id] = name;
-      sockServer.sockets.emit("members", members);
+      sockServer.sockets.emit("members", hash2list(members));
     });
     sockClient.on('msg', function(msg){ 
       sockClient.get('name', function (err, name) {
@@ -71,7 +107,8 @@ var Counter = mongoose.model('Counter', CounterSchema);
     });
     sockClient.on('disconnect', function () {
       delete members[sockClient.id];
-    })
+      sockServer.sockets.emit("members", hash2list(members));
+    });
   });
 
   console.log("http server running at http://localhost:" + port + "/ (" + root + ")");
